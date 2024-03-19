ee4c18479c4fcfca30084bd1d0e7389080a1d63f
Terry <99769575+terrymeow@users.noreply.github.com>
2024-03-19 19:13:55 +0100

    Seminar 6: knex + sqlite

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ A simple TODO list written in Node.js/Express for learning purposes.
 ## Run in dev mode
 ```
 npm install
+npx knex migrate:up
 npm run dev
 ```
--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 import express from 'express'
+import knex from 'knex'
+import knexfile from './knexfile.js';
 
 const app = express();
-
-let todos = [];
-let idCounter = 1;
+const db = knex(knexfile);
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-const getTodo = (req, res, next) => {
-	res.locals.todo = todos.find(todo => todo.id === Number(req.params.id));
+const getTodo = async (req, res, next) => {
+	res.locals.todo = await db('todos').select('*').where('id', req.params.id).first();
 	if (!res.locals.todo) {
 		return res.status(404).send("This TODO doesn't exist!");
 	}
 	next();
 };
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+	const todos = await db('todos').select('*');
 	res.render('todo-list', {
 		todos,
 	});
 });
 
-app.post('/add-todo', (req, res) => {
+app.post('/add-todo', async (req, res) => {
 	const title = req.body.title.trim();
 	if (title.length > 0) {
 		const todo = {
-			id: idCounter++,
 			title: req.body.title,
 			done: false,
 		};
-		todos.push(todo);
+		await db('todos').insert(todo);
 	}
 	res.redirect(303, '/');
 });
@@ -43,21 +43,21 @@ app.get('/todo/:id', getTodo, (req, res) => {
 	});
 });
 
-app.post('/rename-todo/:id', getTodo, (req, res) => {
+app.post('/rename-todo/:id', getTodo, async (req, res) => {
 	const newTitle = req.body.title.trim();
 	if (newTitle.length > 0) {
-		res.locals.todo.title = req.body.title;
+		await db('todos').update({ title: newTitle }).where('id', res.locals.todo.id);
 	}
 	res.redirect(303, 'back');
 })
 
-app.get('/toggle-done/:id', getTodo, (req, res) => {
-	res.locals.todo.done = !res.locals.todo.done;
+app.get('/toggle-done/:id', getTodo, async (req, res) => {
+	await db('todos').update({ done: !res.locals.todo.done }).where('id', res.locals.todo.id);
 	res.redirect(303, 'back');
 });
 
-app.get('/remove-todo/:id', (req, res) => {
-	todos = todos.filter(todo => todo.id !== Number(req.params.id));
+app.get('/remove-todo/:id', async (req, res) => {
+	await db('todos').where('id', req.params.id).del();
 	res.redirect(303, '/');
 });
 
